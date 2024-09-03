a02dd72520ee61470e1dfcb6b317d8311ac0ca29
marianovungel <vungem88@gmail.com>
2024-09-03 08:43:10 -0300

    backend nexus

---
+++ models/docModel.js
@@ -0,0 +1,20 @@
+var mongoose = require("mongoose")
+
+const docSchema = new mongoose.Schema({
+    title:String,
+    content:{
+        type: String,
+        default: ""
+    },
+    uploadedBy:String,
+    date:{
+        type:Date,
+        default: Date.now
+    },
+    lastUpdate:{
+        type:Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model("Document", docSchema)
---
+++ routes/index.js
@@ -0,0 +1,140 @@
+var express = require('express');
+var router = express.Router();
+var userModel = require('../models/useModel');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+var docModel = require('../models/docModel');
+
+const secret = "nexussecret"
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/singUp', async (req, res) => {
+  
+  let { name, email, password, phone, username} = req.body;
+  let emailCon = await userModel.findOne({ email: email});
+  let phoneCon = await userModel.findOne({ phone: phone});
+
+  if (emailCon) {
+    return res.json({ sucess: false, message: "Email already exist!"})
+  } else if (phoneCon) {
+    return res.json({ sucess: false, message: "Phone number already exist!"})
+  }else{
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(password, salt, async function(err, hash) {
+        if(err) throw err;
+        let user = await userModel.create({
+          username:username,
+          nema: name,
+          email:email,
+          phone:phone,
+          password:hash
+        })
+        res.json({ success: true, message: "User created seccessfully!", userId: user._id})
+      });
+    });
+    
+  }
+  
+});
+
+router.post('/login', async (req, res) => {
+  
+  let { email, password} = req.body;
+  let user = await userModel.findOne({ email: email});
+  
+  if (user) {
+    bcrypt.compare(password, user.password, function(err, result){
+      if(result){
+        var token = jwt.sign({ email: user.email, userId: user._id }, secret);
+        res.json({ success: true, message: "Login seccessfully!", userId: user._id, token: token})
+      }else{
+        return res.json({ sucess: false, message: "Invalid Password!"})
+      }
+    })
+  }else{
+    return res.json({ sucess: false, message: "Invalid Email!"}) 
+  }
+
+});
+
+router.post('/createDoc', async (req, res) => {
+  let { userId, docName} = req.body;
+  let user = userModel.findById(userId)
+
+  if(user){
+    let doc = await docModel.create({
+      uploadedBy: userId,
+      title: docName
+    })
+
+    return res.json({ success: true, message: "Documento criado com Sucesso!", docId: doc._id})
+  }else{
+    return res.json({ sucess: false, message: "Usuário Inválido"})
+  }
+
+});
+
+router.post('/uploadDoc', async (req, res) => {
+  let { userId, docId, content} = req.body;
+  let user = userModel.findById(userId)
+  if(user){
+    let doc = await docModel.findByIdAndUpdate(docId, {content: content})
+    return res.json({ success: true, message: "Documento atualizado com Sucesso!"})
+  }else{
+    return res.json({ sucess: false, message: "Usuário Inválido"})
+  }
+
+});
+
+router.post('/getDoc', async (req, res) => {
+  let { userId, docId } = req.body;
+  let user = userModel.findById(userId)
+  if(user){
+    let doc = await docModel.findById(docId)
+    if(doc){
+      return res.json({ success: true, message: "Documento atualizado com Sucesso!", doc: doc});
+    }else{
+      return res.json({ sucess: false, message: "Documento Inválido"})
+    }
+  }else{
+    return res.json({ sucess: false, message: "Usuário Inválido"})
+  }
+
+});
+router.post('/deleteDoc', async (req, res) => {
+  let { userId, docId } = req.body;
+  let user = userModel.findById(userId)
+  if(user){
+    let doc = await docModel.findByIdAndDelete(docId)
+    return res.json({ success: true, message: "Documento Deletado com Sucesso!"});
+  }else{
+    return res.json({ sucess: false, message: "Usuário Inválido"})
+  }
+});
+
+router.post('/getAllDocs', async (req, res) => {
+  let { userId } = req.body;
+  let user = userModel.findById(userId)
+  if(user){
+    let docs = await docModel.find({uploadedBy: userId})
+    return res.json({ success: true, message: "Documents buscados com Sucesso!", docs: docs});
+  }else{
+    return res.json({ sucess: false, message: "Usuário Inválido"})
+  }
+});
+
+router.post('/getUser', async (req, res) => {
+  let { userId } = req.body;
+  let user = userModel.findById(userId)
+  if(user){
+    return res.json({ success: true, message: "Usuário Encontrado com Sucesso!", user: user});
+  }else{
+    return res.json({ sucess: false, message: "Usuário Inválido"})
+  }
+});
+
+module.exports = router;

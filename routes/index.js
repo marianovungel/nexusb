1c3528e9333546b3c867fbbba90e0349ea207618
marianovungel <vungem88@gmail.com>
2024-09-04 11:00:00 -0300

    get user back

--- routes/index.js
+++ routes/index.js
@@ -131,9 +131,9 @@ router.post('/getAllDocs', async (req, res) => {
   }
 });
 
-router.post('/getUser', async (req, res) => {
+router.post('/getSingleUser', async (req, res) => {
   let { userId } = req.body;
-  let user = userModel.findById(userId)
+  let user = userModel.find({userid: userId})
   if(user){
     return res.json({ success: true, message: "Usuário Encontrado com Sucesso!", user: user});
   }else{

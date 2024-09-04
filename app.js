cc0f7f7ea626c1833a515f8f89c5b747a6f773ce
marianovungel <vungem88@gmail.com>
2024-09-04 12:53:44 -0300

    corss

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next)=>{
-  res.header("Access-Control-Allow-Origin", "/*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
   res.header("Access-Control-Allow-Methods", 'GET,POST');
   app.use(cors())
   next();

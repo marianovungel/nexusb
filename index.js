cd6030209471de128092ebfc172fc1d4c7db6734
marianovungel <77215570+marianovungel@users.noreply.github.com>
2024-10-26 18:02:45 -0300

    Update index.js
    Cors Permission

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ app.set('port', port);
 var server = http.createServer(app);
 
 const io = require("socket.io")(server, {
-  cors:{
-      origin: "*",
-      methods: ["GET", "POST"]
-  }
+  cors: {
+    origin: ["http://localhost:3000", "https://lastnexus.vercel.app"], // Permite solicitações apenas desse endereço
+    methods: ["GET", "POST"],         // Métodos permitidos
+    allowedHeaders: ["Content-Type"],  // Cabeçalhos permitidos (opcional)
+    credentials: true,                 // Permite cookies ou credenciais (opcional)
+  },
 })
-
-// app.use(cors())
 app.use((req, res, next)=>{
   res.header("Access-Control-Allow-Origin", "/*");
   res.header("Access-Control-Allow-Methods", 'GET,POST');
@@ -37,6 +37,8 @@ app.use((req, res, next)=>{
   next();
 })
 app.use(cors())
+
+
 
 io.on('connection', (socket)=>{
   socket.emit('me', socket.id);
